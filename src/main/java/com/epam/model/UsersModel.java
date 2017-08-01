de59ae09b8f3c3b6bbfd5a0c42eb30ab2a6c3308
oleksavolodymyr <oleksavolodymyr@gmail.com>
2017-08-01 18:48:16 +0300

    add cucumber test

--- src/main/java/com/epam/model/UsersModel.java
+++ src/main/java/com/epam/model/UsersModel.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/com/epam/model/UsersModel.java
 package com.epam.model;
 
 import java.util.List;
@@ -20,26 +19,3 @@ public class UsersModel {
 	}
 
 }
-=======
-package com.epam.model;
-
-import java.util.List;
-
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "users")
-public class Users {
-	private List<UserModel> users = null;
-
-	@XmlElement(name = "user")
-	public List<UserModel> getUsers() {
-		return this.users;
-	}
-
-	public void setUsers(List<UserModel> users) {
-		this.users = users;
-	}
-
-}
->>>>>>> faa2e69c9d394942576889f588433aac5413dff5:src/main/java/com/epam/model/Users.java
